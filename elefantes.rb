456dda50071bd801a4129c0615a408e792333e68
codeamac07 <codeamac07@gmail.com>
2015-09-02 12:12:26 -0500

    cancion elefantes

---
+++ elefantes.rb
@@ -0,0 +1,8 @@
+def cancionElefantes(tope)
+  num=1
+  while num<tope+1
+    puts "#{num} elefante se columpiaba sobre la tela de una araña, como veía que resistía fueron a llamar a otro elefante."
+    num+=1
+  end
+end
+cancionElefantes(99)
